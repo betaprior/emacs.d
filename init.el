23a7e0594e4cbe3f41ca07d8f2b269369c70402d
Leo Alekseyev <leo@matroskin.(none)>
2009-12-02 20:04:26 -0500

    ===

--- init.el
+++ init.el
@@ -1226,7 +1226,7 @@ With argument, do this that many times."
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(TeX-electric-escape t)
+ '(TeX-electric-escape nil)
  '(TeX-output-view-style TeX-output-view-style-commands)
  '(cygwin-mount-cygwin-bin-directory "c:\\cygwin\\bin")
  '(desktop-save-mode t)
