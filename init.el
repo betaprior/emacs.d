a15ebaac6725bb1623677e84ad38f21c6906d58e
Leo Alekseyev <leo.alekseyev@gmail.com>
2011-02-03 22:17:25 -0500

    fixed shell-dwim

--- init.el
+++ init.el
@@ -556,29 +556,42 @@ the grep command in R"
            (insert (current-kill 0)))))
 
 ;;{{{ Useful UDFs (not written by me)
+(defun my-filter (condp lst)
+  "Stolen from emacswiki. Sample usage:
+(my-filter (lambda (x) (string-match \"^\\*shell\\*\" (buffer-name x))) (buffer-list))"
+    (delq nil
+          (mapcar (lambda (x) (and (funcall condp x) x)) lst)))
+
+(defun rotate-list (list count)
+  "Rotate the LIST by COUNT elements"
+  (cond
+   ((= count 0) list)
+   ((not list) list)
+   (t (rotate-list (nconc  (cdr list) (list (car list)) '()) (1- count)))))
+;; The following is inspired by 
 ;; http://www.emacswiki.org/emacs/ShellMode#toc3
 ;; Note also that you'll want to customize same-window-regexps
 ;; to include "\\*shell.*\\*\\(\\|<[0-9]+>\\)"
 (defun shell-dwim (&optional create)
-   "Start or switch to an inferior shell process, in a smart way.
- If a buffer with a running shell process exists, simply switch to
- that buffer.
- If a shell buffer exists, but the shell process is not running,
- restart the shell.
- If already in an active shell buffer, switch to the next one, if
- any.
- With prefix argument CREATE always start a new shell."
+   "Start or switch to an inferior shell process, in a smart way.  If a
+ buffer with a running shell process exists, simply switch to that buffer.
+ If a shell buffer exists, but the shell process is not running, restart the
+ shell.  If already in an active shell buffer, switch to the next one, if
+ any.  With prefix argument CREATE always start a new shell."
    (interactive "P")
-   (let* ((next-shell-buffer
-           (catch 'found
-             (dolist (buffer (reverse (buffer-list)))
-               (when (and (string-match "^\\*shell\\*" (buffer-name buffer))
-			  (not (string= (buffer-name)
-					(buffer-name buffer))))
-                 (throw 'found buffer)))))
-          (buffer (if create
-                      (generate-new-buffer-name "*shell*")
-                    next-shell-buffer)))
+   (let ((next-shell-buffer) (buffer) 
+	 (shell-buf-list (identity ;;used to be reverse
+			  (sort 
+			   (my-filter (lambda (x) (string-match "^\\*shell\\*" (buffer-name x))) (buffer-list))
+			   '(lambda (a b) (string< (buffer-name a) (buffer-name b)))))))
+     (setq next-shell-buffer 
+	   (if (string-match "^\\*shell\\*" (buffer-name buffer))
+	       (get-buffer (cadr (member (buffer-name) (mapcar (function buffer-name) (append shell-buf-list shell-buf-list)))))
+	     nil))
+     (setq buffer
+	   (if create
+	       (generate-new-buffer-name "*shell*")
+	     next-shell-buffer))
      (shell buffer)))
 
 ;; Tassilo Horn's zap-to-char improvements:
