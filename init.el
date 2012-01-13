9a8492cc578719f26f2754af1e9755c89d1b3f8a
Leo Alekseyev <leo.alekseyev+fj@gmail.com>
2012-01-13 05:42:44 -0600

    updated org, added eimp.el and rainbow-mode.el

--- init.el
+++ init.el
@@ -35,10 +35,10 @@
 ;; (load-file "/home/leo/.emacs.d/init.el.el")
 
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(TeX-electric-escape nil)
  '(TeX-output-view-style TeX-output-view-style-commands)
  '(TeX-view-program-list (quote (("((\"Ghostview\" \"'C:/Program Files/Ghostgum/gsview/gsview32.exe' %o\"))" ""))) t)
@@ -87,10 +87,10 @@
  '(winner-ring-size 100)
  '(x-select-enable-clipboard t))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(hs-face ((nil (:box nil)))))
 
 (put 'narrow-to-region 'disabled nil)
